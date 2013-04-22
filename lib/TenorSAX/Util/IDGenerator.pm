eaa5a716e0519979cb97bab84ae207ac99a8ae2e
brian m. carlson <sandals@crustytoothpaste.net>
2013-04-22 03:06:50 +0000

    Add an ID generator.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ lib/TenorSAX/Util/IDGenerator.pm
@@ -0,0 +1,113 @@
+package TenorSAX::Util::IDGenerator;
+
+use v5.14;
+use strict;
+use warnings;
+
+use warnings qw/FATAL utf8/;
+use utf8;
+no feature 'unicode_strings';
+
+use Moose;
+use namespace::autoclean;
+use Digest::SHA;
+
+has 'seed' => (
+	is => 'ro',
+	default => sub {
+		require Bytes::Random::Secure;
+		return Bytes::Random::Secure::random_bytes(32);
+	},
+);
+has 'counter' => (
+	is => 'rw',
+	default => 0,
+);
+has 'version' => (
+	is => 'ro',
+	default => 0,
+);
+has 'key' => (
+	is => 'rw',
+	default => undef,
+);
+has 'prefix' => (
+	is => 'ro',
+	default => 'tsid',
+);
+has 'length' => (
+	is => 'ro',
+	default => 20,
+);
+
+# Hashes distinctly for undef and all defined values.  Numbers are converted to
+# their string representation.
+sub _hash_value {
+	my ($self, $value) = @_;
+
+	# This is the SHA-256 hash of "0".
+	return
+		"\x5f\xec\xeb\x66\xff\xc8\x6f\x38\xd9\x52\x78\x6c\x6d\x69\x6c\x79" .
+		"\xc2\xdb\xc2\x39\xdd\x4e\x91\xb4\x67\x29\xd7\x3a\x27\xfb\x57\xe9"
+		unless defined $value;
+	return Digest::SHA::sha256("1$value");
+}
+
+sub _init_key {
+	my ($self) = @_;
+
+	return if defined $self->key;
+
+	$self->key(Digest::SHA::sha256(join("", map { $self->_hash_value($_) }
+				($self->version, $self->seed, $self->prefix))));
+
+	return;
+};
+
+=head1 Methods
+
+=over 4
+
+=item $idg->id(%attrs)
+
+Generates an ID suitable for use with in an xml:id attribute.  attrs can
+include:
+
+=over 4
+
+=item text
+
+The text over which to process the data.  This is optional.
+
+=back
+
+If no attributes are provided, the overhead is that of three single-block
+SHA-256 hashes.  The algorithm used does not guarantee unique IDs.  However, the
+likelihood of generating two identical IDs is 1 in 2**(6*C<length>), which is
+extremely unlikely.  There is a similar, but weaker guarantee, if the ID is
+treated case-insensitively.
+
+=cut
+
+sub id {
+	my ($self, %attrs) = @_;
+
+	$self->_init_key;
+
+	my $rand_val = Digest::SHA::hmac_sha256_base64(join("", map {
+				$self->_hash_value($_) 
+			} ($attrs{text}, $self->counter)), $self->key);
+	$rand_val =~ tr{+/}{_.};
+	$self->counter($self->counter + 1);
+	
+	return $self->prefix . substr($rand_val, 0, $self->length);
+}
+
+=back
+
+=cut
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/idg.t
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use v5.14;
+use warnings;
+use strict;
+use open qw/:encoding(UTF-8) :std/;
+use utf8;
+
+use Test::More tests => 18 + 1;
+use Test::NoWarnings;
+use TenorSAX::Util::IDGenerator;
+use Digest::SHA		qw/sha256 sha256_hex/;
+use Encode;
+
+my $gen = TenorSAX::Util::IDGenerator->new(seed => 'abc');
+
+is($gen->version, 0, "default version is 0");
+is($gen->length, 20, "default length is correct");
+is($gen->counter, 0, "counter starts at 0");
+
+my %saved_values; 
+note "Testing _hash_value";
+my $hashed = $gen->_hash_value(undef);
+is($hashed, sha256("0"), "undef hashes to 0");
+$saved_values{$hashed} = undef;
+my @input_values = (qw/0 15 Monkeys © undef/, "\N{U+00c2}\N{U+00a9}");
+foreach my $text (@input_values) {
+	$hashed = $gen->_hash_value($text);
+	$saved_values{$hashed} = $text;
+	is($hashed, sha256("1$text"), "'$text' hashes as expected");
+}
+is(scalar @input_values + 1, scalar keys %saved_values,
+	"all values are unique");
+
+my $expected_key = "\x8d\x28\x74\xa1\x6f\xe7\x89\x64\xaa\x8e\x44\x47\x90\x28" .
+	"\xa8\x89\x28\xb9\x9b\x43\x48\x5e\x72\x09\xc0\xe9\x19\xf3\x1e\xa3\x9c\x65";
+is($gen->key, undef, "key starts off undef");
+is($gen->id, "tsidPlLW4czAg4Lgjv.jAV1y", "id is as expected");
+is($gen->key, $expected_key, "key is now set");
+is($gen->counter, 1, "counter is 1");
+is($gen->id, "tsidPdfYO1YjuJw75m.8imWO", "second id is as expected");
+is($gen->key, $expected_key, "key does not change");
+is($gen->counter, 2, "counter is now 2");
