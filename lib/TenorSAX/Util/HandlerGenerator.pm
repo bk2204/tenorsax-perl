11b7272b9adac89fb50a87a32173b3d1a248381c
brian m. carlson <sandals@crustytoothpaste.net>
2013-05-27 00:39:17 +0000

    Add POD to HandlerGenerator.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/TenorSAX/Util/HandlerGenerator.pm
+++ lib/TenorSAX/Util/HandlerGenerator.pm
@@ -10,6 +10,39 @@ use utf8;
 use Moose;
 use namespace::autoclean;
 
+=head1 Methods
+
+=over 4
+
+=item $hg->generate(@stack)
+
+Takes a list of hashrefs defining a series of SAX filters and writers and
+returns the first item in the chain.  The writer should be the last item in the
+list.  Each hashref may have one or more of the following items:
+
+=over 4
+
+=item name (mandatory)
+
+The name of the filter or writer class.  This will be passed to require, so do
+not pass untrusted input.
+
+=item attributes
+
+A hashref of attributes to pass to the constructor.
+
+=item methods
+
+An arrayref of methods to call on the constructed object.  This can be useful
+when the filter needs additional input, such as with XML::Filter::XSLT.  Each
+item should be a hashref with the key B<name> providing the method name and
+B<args> providing the list of arguments to be passed to the method.
+
+=back
+
+=cut
+
+
 sub generate {
 	my ($self, @stack) = @_;
 	my $previous;
@@ -32,6 +65,10 @@ sub generate {
 
 	return $previous;
 }
+
+=back
+
+=cut
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
